6a24eaf08900899be5333d4e9601105c7a871875
Mikhail Martynenko <michael@Mikhails-MacBook-Pro.local>
2019-05-26 00:01:09 +0300

    Update urls.py

--- url_count/urls.py
+++ url_count/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('list/', views.url_list, name='list'),
-    path("create/", views.url_create, name='create')
+    path("create/", views.url_create, name='create'),
     path("add-url/", views.add_url, name="api-add-url"),
 ]
