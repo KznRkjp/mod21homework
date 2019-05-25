021e34d2f52dc20cf93959167242258b73180011
Mikhail Martynenko <michael@Mikhails-MacBook-Pro.local>
2019-05-25 23:04:57 +0300

    Update admin.py

--- url_count/admin.py
+++ url_count/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from url_count.models import Url
 
-admin.site.register(Url)
-# Register your models here.
+@admin.register(TodoItem)
+class UrlCountAdmin(admin.ModelAdmin):
+    list_display = ('job','url_link', 'status', 'date')
