df3335000ba83504a77ead65aece2e314becf5c2
Mikhail Martynenko <michael@Mikhails-MacBook-Pro.local>
2019-05-25 22:49:40 +0300

    model

--- url_count/models.py
+++ url_count/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Url(models.Model):
+    job = models.UUIDField()
+    date = models.DateTimeField(auto_now_add=True)
+    url_link = models.CharField(max_length=128)
+    word = models.CharField(max_length=64)
+    status = models.BooleanField("выполнено", default=False)
+    last_update = models.DateTimeField(auto_now=True)
+    result = models.CharField(max_length=64)
+
+    #description = models.CharField(max_length=64)
+    #is_completed = models.BooleanField("выполнено", default=False)
+
+    def __str__(self):
+        return self.url_link.lower()
+    class Meta:
+        ordering = ('-date',)
