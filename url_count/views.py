f1ea1a122de5d526501101749846ab2fd8049fbe
Mikhail Martynenko <michael@Mikhails-MacBook-Pro.local>
2019-05-26 00:12:00 +0300

    Update views.py

--- url_count/views.py
+++ url_count/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from url_count.models import Url
+from django.shortcuts import redirect
 
 def index(request):
     return HttpResponse("Примитивный ответ из приложения tasks")
