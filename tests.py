d5cb4b6c782a6dbb303980676ea65ace391fae4d
KznRkjp <46960704+KznRkjp@users.noreply.github.com>
2019-05-27 13:29:07 +0300

    Update tests.py

--- tests.py
+++ tests.py
@@ -12,10 +12,10 @@ q = Queue(connection=redis_conn)
 print("*"*85)
 for url in urls:
     job = q.enqueue(count_words_at_url, url)
-    while job.result is None:
-        time.sleep(1)
+while job.result is None:
+    time.sleep(1)
 
-    print(job.result)
+print(job.result)
 
 
 #while job.result is None:
