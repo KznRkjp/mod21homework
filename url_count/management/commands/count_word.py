724698c394f14311a69ac827dd1d316940715e58
KznRkjp <46960704+KznRkjp@users.noreply.github.com>
2019-05-27 15:24:35 +0300

    Update count_word.py

--- url_count/management/commands/count_word.py
+++ url_count/management/commands/count_word.py
@@ -34,10 +34,7 @@ class Command(BaseCommand):
                 t.save
 
             job_list.append(t.job)
-            jobs = q.enqueue(test_func.count_words_at_url,
-                            args=(t.url_link,t.word,),
-                            kwargs={'job_id'=t.job,
-                            })
+            jobs = q.enqueue(test_func.count_words_at_url,(t.url_link,t.word,),job_id=t.job)
         while len(job_list)>0:
             for task in job_list:
                 job = q.fetch_job(task)
