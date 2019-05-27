91dd836ca504fcc44ce2a5bcf94c89843978de74
KznRkjp <46960704+KznRkjp@users.noreply.github.com>
2019-05-27 13:16:32 +0300

    Update tests.py

--- tests.py
+++ tests.py
@@ -2,11 +2,21 @@ import time
 
 from rq import Queue
 from redis import Redis
+import requests
 
+def count_words_at_url(url):
+    resp = requests.get(url)
+    return len(resp.text.split())
+
+urls = ["http://mail.ru","http://yandex.ru","http://google.com"]
+
+
+reddis_conn=Redis()
 q = Queue(connection=Redis())
-job = q.enqueue(len, ["Анна", "Мария", "Валентина"])
+for url in urls:
+    job = q.enqueue(count_words_at_url, url)
 
 while job.result is None:
     time.sleep(1)
 
-print(job.result) 
+print(job.result)
