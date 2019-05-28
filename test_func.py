cbac311950a3514a7ec740fce2ee7734c5fa3396
KznRkjp <46960704+KznRkjp@users.noreply.github.com>
2019-05-28 14:43:56 +0300

    Update test_func.py

--- test_func.py
+++ test_func.py
@@ -1,8 +1,9 @@
 import requests
 
 def count_words_at_url(url,word):
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
     try:
-        resp = requests.get(url)
+        resp = requests.get(url, headers=headers)
         if resp.status_code == 200:
             count = 0
             for i in resp.text.split():
