f4dd628e62ffd2310a5dc013db67b1d2e43cf203
KznRkjp <46960704+KznRkjp@users.noreply.github.com>
2019-05-28 13:29:40 +0300

    Update count_word.py

--- url_count/management/commands/count_word.py
+++ url_count/management/commands/count_word.py
@@ -74,7 +74,8 @@ class Command(BaseCommand):
                         obj.status = True
                         obj.last_update = datetime.now(timezone.utc)
                         obj.save()
-                        job_list.remove(task)
+                        if task in job_list:
+                            job_list.remove(task)
                         continue
                 obj = Url.objects.get(job=task)
 
